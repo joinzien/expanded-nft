fb694f999487043727d7eeb9c5dcdd0709f77c9b
Zoe Nolan <zoe@zien.io>
2022-05-23 00:08:41 +0100

    Split out the royalty tests

---
+++ test/MembershipRoyaltyTest.ts
@@ -0,0 +1,95 @@
+import { expect } from "chai";
+import "@nomiclabs/hardhat-ethers";
+import { ethers, deployments } from "hardhat";
+import parseDataURI from "data-urls";
+
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import {
+  SingleEditionMintableCreator,
+  SingleEditionMintable,
+} from "../typechain";
+
+describe("SingleEditionMintable", () => {
+  let signer: SignerWithAddress;
+  let signerAddress: string;
+  let dynamicSketch: SingleEditionMintableCreator;
+
+  beforeEach(async () => {
+    const { SingleEditionMintableCreator } = await deployments.fixture([
+      "SingleEditionMintableCreator",
+      "SingleEditionMintable",
+    ]);
+    const dynamicMintableAddress = (
+      await deployments.get("SingleEditionMintable")
+    ).address;
+    dynamicSketch = (await ethers.getContractAt(
+      "SingleEditionMintableCreator",
+      SingleEditionMintableCreator.address
+    )) as SingleEditionMintableCreator;
+
+    signer = (await ethers.getSigners())[0];
+    signerAddress = await signer.getAddress();
+  });
+  describe("with a edition", () => {
+    let signer1: SignerWithAddress;
+    let minterContract: SingleEditionMintable;
+    beforeEach(async () => {
+      signer1 = (await ethers.getSigners())[1];
+      await dynamicSketch.createEdition(
+        "Testing Token",
+        "TEST",
+        "This is a testing token for all",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        10,
+        10
+      );
+
+      const editionResult = await dynamicSketch.getEditionAtId(0);
+      minterContract = (await ethers.getContractAt(
+        "SingleEditionMintable",
+        editionResult
+      )) as SingleEditionMintable;
+    });
+    describe("royalty 2981", () => {
+      it("follows royalty payout for owner", async () => {
+        await minterContract.mintEdition(signerAddress);
+        // allows royalty payout info to be updated
+        expect((await minterContract.royaltyInfo(1, 100))[0]).to.be.equal(
+          signerAddress
+        );
+        await minterContract.transferOwnership(await signer1.getAddress());
+        expect((await minterContract.royaltyInfo(1, 100))[0]).to.be.equal(
+          await signer1.getAddress()
+        );
+      });
+      it("sets the correct royalty amount", async () => {
+        await dynamicSketch.createEdition(
+          "Testing Token",
+          "TEST",
+          "This is a testing token for all",
+          "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+          "0x0000000000000000000000000000000000000000000000000000000000000000",
+          "",
+          "0x0000000000000000000000000000000000000000000000000000000000000000",
+          // 2% royalty since BPS
+          200,
+          200
+        );
+    
+        const editionResult = await dynamicSketch.getEditionAtId(1);
+        const minterContractNew = (await ethers.getContractAt(
+          "SingleEditionMintable",
+          editionResult
+        )) as SingleEditionMintable;
+
+        await minterContractNew.mintEdition(signerAddress);
+        expect((await minterContractNew.royaltyInfo(1, ethers.utils.parseEther("1.0")))[1]).to.be.equal(
+          ethers.utils.parseEther("0.02")
+        );
+      });
+    });
+  });
+});
--- test/SingleEditionMintableTest.ts
+++ test/SingleEditionMintableTest.ts
@@ -9,7 +9,7 @@ import {
   SingleEditionMintable,
 } from "../typechain";
 
-describe("SingleEditionMintable", () => {
+describe("Membership", () => {
   let signer: SignerWithAddress;
   let signerAddress: string;
   let dynamicSketch: SingleEditionMintableCreator;
@@ -80,44 +80,6 @@ describe("SingleEditionMintable", () => {
       expect(await minterContract.supportsInterface("0x01ffc9a7")).to.be.true;
       // ERC721 interface
       expect(await minterContract.supportsInterface("0x80ac58cd")).to.be.true;
-    });
-    describe("royalty 2981", () => {
-      it("follows royalty payout for owner", async () => {
-        await minterContract.mintEdition(signerAddress);
-        // allows royalty payout info to be updated
-        expect((await minterContract.royaltyInfo(1, 100))[0]).to.be.equal(
-          signerAddress
-        );
-        await minterContract.transferOwnership(await signer1.getAddress());
-        expect((await minterContract.royaltyInfo(1, 100))[0]).to.be.equal(
-          await signer1.getAddress()
-        );
-      });
-      it("sets the correct royalty amount", async () => {
-        await dynamicSketch.createEdition(
-          "Testing Token",
-          "TEST",
-          "This is a testing token for all",
-          "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
-          "0x0000000000000000000000000000000000000000000000000000000000000000",
-          "",
-          "0x0000000000000000000000000000000000000000000000000000000000000000",
-          // 2% royalty since BPS
-          200,
-          200
-        );
-    
-        const editionResult = await dynamicSketch.getEditionAtId(1);
-        const minterContractNew = (await ethers.getContractAt(
-          "SingleEditionMintable",
-          editionResult
-        )) as SingleEditionMintable;
-
-        await minterContractNew.mintEdition(signerAddress);
-        expect((await minterContractNew.royaltyInfo(1, ethers.utils.parseEther("1.0")))[1]).to.be.equal(
-          ethers.utils.parseEther("0.02")
-        );
-      });
     });
     it("mints a large batch", async () => {
       // no limit for edition size
