93ff18d9c885914f705433d28111c22828e92b6c
Zoe Nolan <zoe@zien.io>
2022-05-23 00:17:32 +0100

    Split out the mint tests

---
+++ test/MembershipMintTest.ts
@@ -0,0 +1,136 @@
+import { expect } from "chai";
+import "@nomiclabs/hardhat-ethers";
+import { ethers, deployments } from "hardhat";
+import parseDataURI from "data-urls";
+
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import {
+  SingleEditionMintableCreator,
+  SingleEditionMintable,
+} from "../typechain";
+
+describe("Membership", () => {
+  let signer: SignerWithAddress;
+  let signerAddress: string;
+  let dynamicSketch: SingleEditionMintableCreator;
+
+  beforeEach(async () => {
+    const { SingleEditionMintableCreator } = await deployments.fixture([
+      "SingleEditionMintableCreator",
+      "SingleEditionMintable",
+    ]);
+    const dynamicMintableAddress = (
+      await deployments.get("SingleEditionMintable")
+    ).address;
+    dynamicSketch = (await ethers.getContractAt(
+      "SingleEditionMintableCreator",
+      SingleEditionMintableCreator.address
+    )) as SingleEditionMintableCreator;
+
+    signer = (await ethers.getSigners())[0];
+    signerAddress = await signer.getAddress();
+  });
+  describe("with a edition", () => {
+    let signer1: SignerWithAddress;
+    let minterContract: SingleEditionMintable;
+    beforeEach(async () => {
+      signer1 = (await ethers.getSigners())[1];
+      await dynamicSketch.createEdition(
+        "Testing Token",
+        "TEST",
+        "This is a testing token for all",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        10,
+        10
+      );
+
+      const editionResult = await dynamicSketch.getEditionAtId(0);
+      minterContract = (await ethers.getContractAt(
+        "SingleEditionMintable",
+        editionResult
+      )) as SingleEditionMintable;
+    });
+    it("mints a large batch", async () => {
+      // no limit for edition size
+      await dynamicSketch.createEdition(
+        "Testing Token",
+        "TEST",
+        "This is a testing token for all",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        0,
+        0
+      );
+
+      const editionResult = await dynamicSketch.getEditionAtId(1);
+      minterContract = (await ethers.getContractAt(
+        "SingleEditionMintable",
+        editionResult
+      )) as SingleEditionMintable;
+
+      const [s1, s2, s3] = await ethers.getSigners();
+      const [s1a, s2a, s3a] = [
+        await s1.getAddress(),
+        await s2.getAddress(),
+        await s3.getAddress(),
+      ];
+      const toAddresses = [];
+      for (let i = 0; i < 100; i++) {
+        toAddresses.push(s1a);
+        toAddresses.push(s2a);
+        toAddresses.push(s3a);
+      }
+      await minterContract.mintEditions(toAddresses);
+    });
+    it("stops after editions are sold out", async () => {
+      const [_, signer1] = await ethers.getSigners();
+
+      expect(await minterContract.numberCanMint()).to.be.equal(10);
+
+      // Mint first edition
+      for (var i = 1; i <= 10; i++) {
+        await expect(minterContract.mintEdition(await signer1.getAddress()))
+          .to.emit(minterContract, "Transfer")
+          .withArgs(
+            "0x0000000000000000000000000000000000000000",
+            await signer1.getAddress(),
+            i
+          );
+      }
+
+      expect(await minterContract.numberCanMint()).to.be.equal(0);
+
+      await expect(
+        minterContract.mintEdition(signerAddress)
+      ).to.be.revertedWith("Sold out");
+
+      const tokenURI = await minterContract.tokenURI(10);
+      const parsedTokenURI = parseDataURI(tokenURI);
+      if (!parsedTokenURI) {
+        throw "No parsed token uri";
+      }
+
+      // Check metadata from edition
+      const uriData = Buffer.from(parsedTokenURI.body).toString("utf-8");
+      console.log({ tokenURI, uriData });
+      const metadata = JSON.parse(uriData);
+
+      expect(parsedTokenURI.mimeType.type).to.equal("application");
+      expect(parsedTokenURI.mimeType.subtype).to.equal("json");
+      expect(JSON.stringify(metadata)).to.equal(
+        JSON.stringify({
+          name: "Testing Token 10/10",
+          description: "This is a testing token for all",
+          animation_url:
+            "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy?id=10",
+          properties: { number: 10, name: "Testing Token" },
+        })
+      );
+    });
+  });
+});
