35386d1c65d46b4e47d2698f515d2029e80104ee
Zoe Nolan <zoe@zien.io>
2023-10-23 22:47:39 +0100

    Test holding both passes

--- test/DiscountTest.ts
+++ test/DiscountTest.ts
@@ -171,4 +171,40 @@ describe("Discounts", () => {
 
   });  
 
+  it("A annual and lifetime pass holder can not mint while the drop is not for sale", async () => {
+    await annualPassContract.connect(user).mint(userAddress);
+    await lifetimePassContract.connect(user).mint(userAddress);  
+    await minterContract.setAllowedMinter(0);
+
+    await expect(minterContract.connect(user).mintEditions([signerAddress], { value: ethers.utils.parseEther("0.1") })).to.be.revertedWith("Needs to be an allowed minter");
+  });  
+
+  it("A annual and lifetime pass holder can mint while the drop is only for sale to allow listed wallets", async () => {
+    await annualPassContract.connect(user).mint(userAddress);
+    await lifetimePassContract.connect(user).mint(userAddress);  
+    await minterContract.setAllowedMinter(1);
+
+    expect(await minterContract.connect(user).mintEditions([signerAddress], { value: ethers.utils.parseEther("0.1") })).to.emit(minterContract, "EditionSold");
+ 
+    expect(await minterContract.totalSupply()).to.be.equal(1);
+    expect(await minterContract.getAllowListMintLimit()).to.be.equal(2);
+    expect(await minterContract.getGeneralMintLimit()).to.be.equal(1);
+    expect(await minterContract.getMintLimit(signerAddress)).to.be.equal(9);   
+  });  
+  
+  it("A annual and lifetime pass holder can not mint while the drop is only for sale to all wallets", async () => {
+    await annualPassContract.connect(user).mint(userAddress);
+    await lifetimePassContract.connect(user).mint(userAddress);      
+    await minterContract.setAllowedMinter(2);
+
+    expect(await minterContract.connect(user).mintEditions([signerAddress], { value: ethers.utils.parseEther("0.1") })).to.emit(minterContract, "EditionSold");
+ 
+    expect(await minterContract.totalSupply()).to.be.equal(1);
+    expect(await minterContract.getAllowListMintLimit()).to.be.equal(2);
+    expect(await minterContract.getGeneralMintLimit()).to.be.equal(1);
+    expect(await minterContract.getMintLimit(signerAddress)).to.be.equal(9);     
+
+  });  
+
+
 });
